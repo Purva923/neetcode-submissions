be416ca1abfa1dd5245c7a85c508a787c63edb14
Purva923 <purvabagul923@gmail.com>
2026-07-18 09:19:27 +0530

    Bulk sync: 3 submissions

---
+++ Data Structures & Algorithms/duplicate-integer/submission-0.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool hasDuplicate(vector<int>& nums) {
+        map <int,int> m;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            m[nums[i]]++;
+            if(m[nums[i]]>1)
+            return true;
+        }
+        return false;
+    }
+};

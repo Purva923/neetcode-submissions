b091719455a0d31176405d3d70d2e8f6892c7bc5
Purva923 <purvabagul923@gmail.com>
2026-07-20 19:41:03 +0530

    Add: trapping-rain-water - submission-10

---
+++ Data Structures & Algorithms/trapping-rain-water/submission-10.cpp
@@ -0,0 +1,42 @@
+class Solution {
+public:
+    int trap(vector<int>& height) {
+        int l=0,r=height.size()-1;
+
+        while(l<r&&height[l+1]>=height[l])
+        l++;
+        while(l<r&&height[r-1]>=height[r])
+        r--;
+
+        int sum=0;
+        int h;
+        while(l<r)
+        {
+            while(l<r&&height[l]<=height[r])
+            {
+                h=height[l];
+                l++;
+                while(l<r&&height[l]<h)
+                {
+                    sum+=h-height[l];
+                    l++;
+                    //while(l<r&&height[l-1]==height[l])
+                    //l++;
+                }
+            }
+            while(l<r&&height[l]>height[r])
+            {
+                h=height[r];
+                r--;
+                while(l<r&&h>height[r])
+                {
+                    sum+=h-height[r];
+                    r--;
+                    //while(l<r&&height[r+1]==height[r])
+                    //r--;
+                }
+            }
+        }
+        return sum;
+    }
+};
